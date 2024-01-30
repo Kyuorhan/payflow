b09e3da21dbe1b95ea868d5ef1473c187aca4f0e
Kyuorhan <jkdevprogrammer@gmail.com>
2024-01-30 16:13:54 -0400

    Added new themes in application

---
+++ lib/src/shared/themes/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFFFF941A);
+  static const grey = Color(0xFF585666);
+  static const delete = Color(0xFFE83F5B);
+  static const heading = Color(0xFF585666);
+  static const body = Color(0xFF706E7A);
+  static const stroke = Color(0xFFE3E3E6);
+  static const shape = Color(0xFFFAFAFC);
+  static const background = Color(0xFFFFFFFF);
+}
