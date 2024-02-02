d95b98fae54d1f7db9440420781a731b4b0cdced
Kyuorhan <jkdevprogrammer@gmail.com>
2024-02-02 00:28:06 -0400

    Update colors themes in application

--- lib/src/shared/themes/app_colors.dart
+++ lib/src/shared/themes/app_colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const primary = Color(0xFFFF941A);
+  static const secundary = Color(0xFFFFC380);
   static const grey = Color(0xFF585666);
   static const delete = Color(0xFFE83F5B);
   static const heading = Color(0xFF585666);
@@ -9,4 +10,7 @@ class AppColors {
   static const stroke = Color(0xFFE3E3E6);
   static const shape = Color(0xFFFAFAFC);
   static const background = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
+  static const black26 = Color(0x42000000);
+  static const transparent = Color(0x00000000);
 }
