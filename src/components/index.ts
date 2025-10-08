60492c29aed9d4ce56d913561e401f8e08e3e1a2
Jhonny Kyuorhan <jk.developrs@gmail.com>
2025-10-08 19:21:02 -0400

    Add shared UI components and hooks
    Introduces reusable components (EditScreenInfo, ExternalLink, StyledText, Themed) and related hooks for color scheme and client-only values. Also adds centralized exports and a test for StyledText. Updates .gitignore for common project files.

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+// Exports centralizados dos componentes
+export { default as EditScreenInfo } from "./EditScreenInfo";
+export { ExternalLink } from "./ExternalLink";
+export { Icon, PayFlowIcons } from "./Icon";
+export { MonoText } from "./StyledText";
+export { Text, View } from "./Themed";
+export { useClientOnlyValue } from "./useClientOnlyValue";
+export { useColorScheme } from "./useColorScheme";
